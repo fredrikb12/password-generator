e7cf6ee1d35c411fd4b415474ad345e1323a43e4
fredrikb12 <fredrik.backman12@gmail.com>
2022-09-12 15:12:26 +0200

    Remove unneeded code

--- src/components/Icon/Icon.styled.js
+++ src/components/Icon/Icon.styled.js
@@ -4,7 +4,6 @@ export const StyledIcon = styled.img`
   width: ${(props) => props.theme.icons[props.iconType].width.regular};
   height: ${(props) => props.theme.icons[props.iconType].height.regular};
   &:hover {
-    ${(props) => console.log(props)}
     filter: ${(props) => props.theme.icons[props.iconType].filters.hover};
     cursor: pointer;
   }
